022558d2e2cfdb190de366b36e9e2917a763b75b
Michael DiBernardo <mikedebo@gmail.com>
2016-07-16 18:06:14 -0400

    Game

--- main.go
+++ main.go
@@ -1,38 +1,10 @@
 package main
 
 import (
-	"github.com/MichaelDiBernardo/srl/lib/math"
-	"github.com/nsf/termbox-go"
+	"github.com/MichaelDiBernardo/srl/lib/game"
 )
 
 func main() {
-	err := termbox.Init()
-	if err != nil {
-		panic(err)
-	}
-	defer termbox.Close()
-
-	x, y := 0, 0
-
-	for {
-		termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-		termbox.SetCell(x, y, '@', termbox.ColorBlue, termbox.ColorBlack)
-		termbox.Flush()
-
-		switch ev := termbox.PollEvent(); ev.Type {
-		case termbox.EventKey:
-			switch ev.Key {
-			case termbox.KeyEsc:
-				return
-			case termbox.KeyArrowUp:
-				y = math.Max(y-1, 0)
-			case termbox.KeyArrowDown:
-				y += 1
-			case termbox.KeyArrowLeft:
-				x = math.Max(x-1, 0)
-			case termbox.KeyArrowRight:
-				x += 1
-			}
-		}
-	}
+    game := game.New()
+    game.Loop()
 }
