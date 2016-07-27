fdbf46e1c02a88fc1eb1a8268be6c4e44f85dfe5
Michael DiBernardo <mikedebo@gmail.com>
2016-07-26 20:19:56 -0400

    Starting to make the console not a disaster.

--- lib/client/console.go
+++ lib/client/console.go
@@ -1,38 +1,45 @@
 package client
 
 import (
+	"container/list"
 	"github.com/MichaelDiBernardo/srl/lib/game"
 	"github.com/nsf/termbox-go"
 )
 
+// A console client.
 type Console struct {
 }
 
-// Looks exactly like termbox.Cell :(
+// A glyph used to render a tile.
 type glyph struct {
 	Ch rune
 	Fg termbox.Attribute
 	Bg termbox.Attribute
 }
 
+// Glyphs used to render actors.
 var actorGlyphs = map[game.ObjSubtype]glyph{
 	"Player": glyph{Ch: '@', Fg: termbox.ColorWhite, Bg: termbox.ColorBlack},
 	"MonOrc": glyph{Ch: 'o', Fg: termbox.ColorGreen, Bg: termbox.ColorBlack},
 }
 
+// Glyphs used to render tiles.
 var featureGlyphs = map[game.FeatureType]glyph{
 	"FeatWall":  glyph{Ch: '#', Fg: termbox.ColorRed, Bg: termbox.ColorBlack},
 	"FeatFloor": glyph{Ch: '.', Fg: termbox.ColorWhite, Bg: termbox.ColorBlack},
 }
 
+// Create a new console client.
 func NewConsole() *Console {
 	return &Console{}
 }
 
+// Init the console client.
 func (c *Console) Init() error {
 	return termbox.Init()
 }
 
+// Render the current screen on this console.
 func (c *Console) Render(g *game.Game) {
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 	for _, row := range g.Level.Map {
@@ -50,12 +57,13 @@ func (c *Console) Render(g *game.Game) {
 	line := 0
 	for !g.Events.Empty() {
 		m := g.Events.Next().(*game.MessageEvent)
-		c.write(0, line, m.Text, termbox.ColorWhite, termbox.ColorBlack)
+		write(0, line, m.Text, termbox.ColorWhite, termbox.ColorBlack)
 		line++
 	}
 	termbox.Flush()
 }
 
+// Get the next command from the player to be sent to the game instance.
 func (c *Console) NextCommand() game.Command {
 	keymap := map[rune]game.Command{
 		'h': game.CommandMoveW,
@@ -78,11 +86,29 @@ func (c *Console) NextCommand() game.Command {
 	}
 }
 
+// Tear down the console client.
 func (c *Console) Close() {
 	termbox.Close()
 }
 
-func (c *Console) write(x, y int, text string, fg termbox.Attribute, bg termbox.Attribute) {
+type messagePanel struct {
+	lines *list.List
+    size int
+}
+
+func newMessagePanel(size int) *messagePanel {
+    return &messagePanel{lines: list.New(), size: size}
+}
+
+func (m *messagePanel) Message(text string) {
+    m.lines.PushBack(text)
+    if m.lines.Len() > m.size {
+        m.lines.Remove(m.lines.Front())
+    }
+}
+
+// Write a string to the console.
+func write(x, y int, text string, fg termbox.Attribute, bg termbox.Attribute) {
 	i := 0
 	for _, r := range text {
 		termbox.SetCell(x+i, y, r, fg, bg)
---
+++ lib/client/console_test.go
@@ -0,0 +1,25 @@
+package client
+
+import (
+	"testing"
+)
+
+func TestMessagePanelHasLimit(t *testing.T) {
+    size := 2
+    sut := newMessagePanel(size)
+    sut.Message("hi")
+    sut.Message("bye")
+    sut.Message("foo")
+
+    if l := sut.lines.Len(); l != size {
+        t.Errorf(`MessagePanel held %d messages, want %d`, l, size)
+    }
+
+    if s := sut.lines.Front().Value.(string); s != "bye" {
+        t.Errorf(`Bottom line was %v, want 'bye'`, s)
+    }
+
+    if s := sut.lines.Back().Value.(string); s != "foo" {
+        t.Errorf(`Bottom line was %v, want 'foo'`, s)
+    }
+}
