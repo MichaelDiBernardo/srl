622da68f164cb70c0031ac0757e316abad59831f
Michael Dibernardo <mikedebo@gmail.com>
2017-01-11 09:59:31 +0800

    FOV v4...

--- lib/game/actor_see.go
+++ lib/game/actor_see.go
@@ -25,23 +25,48 @@ func NewActorSeer(obj *Obj) Seer {
 }
 
 func (a *ActorSeer) CalcFOV() {
-	// Max area of FOV is (2r+1) * (2r+1)
-	maxarea := 4*FOVRadius*FOVRadius + 4*FOVRadius + 1
-	fov := make([]math.Point, maxarea)
+    // TODO: This is basically a direct translation of fcrawl's raycasting FOV
+    // algorithm. I didn't try at all to make it less pythony and more go-ey.
+    // Should replace with something less churny or just a totally different
+    // algorithm, there's enough of these written in C that should be a lot
+    // less impedence-mismatchy to directly translate.
+	fov := newPointSet()
+	fov.Add(math.Origin)
 
 	pos, level := a.Obj().Pos(), a.Obj().Level
 
-	for r := 0; r <= FOVRadius; r++ {
+	// Light begins casting in all directions.
+	light := make(map[math.Point]pointset)
+	light[math.Origin] = newPointSetL(math.ChebyEdge(1))
+
+	for r := 0; r < FOVRadius; r++ {
 		edge := math.ChebyEdge(r)
 		for _, cpt := range edge {
-			pt := pos.Add(cpt)
-			if pt.In(level) {
-				fov = append(fov, pt)
+			li, pt := light[cpt], pos.Add(cpt)
+			if li == nil || !pt.In(level) || level.At(pt).Feature.Opaque {
+				continue
+			}
+
+			for dp, _ := range li {
+				cur := cpt.Add(dp)
+				next := li.Intersect(adj45dirs(dp))
+				light[cur] = light[cur].Union(next)
+				fov.Add(cur)
 			}
 		}
 	}
 
-	a.fov = fov
+	// fov is currently relative to 0,0 as center, and has not yet been
+	// translated to the map coords. We also take this opportunity to coerce
+	// the set into a slice.
+	transfov := make([]math.Point, 0, len(fov))
+	for p, _ := range fov {
+		tpt := p.Add(pos)
+		if tpt.In(level) {
+			transfov = append(transfov, tpt)
+		}
+	}
+	a.fov = transfov
 
 	if actor := a.Obj(); actor.IsPlayer() {
 		actor.Level.UpdateVis()
@@ -50,4 +75,68 @@ func (a *ActorSeer) CalcFOV() {
 
 func (a *ActorSeer) FOV() FOV {
 	return a.fov
+}
+
+func adj45dirs(d math.Point) pointset {
+	dirscircle := []math.Point{
+		math.Pt(-1, 0), math.Pt(-1, -1), math.Pt(0, -1), math.Pt(1, -1),
+		math.Pt(1, 0), math.Pt(1, 1), math.Pt(0, 1), math.Pt(-1, 1),
+	}
+
+	index := -1
+	for i, cpt := range dirscircle {
+		if cpt == d {
+			index = i
+			break
+		}
+	}
+
+	dirs := newPointSet()
+	for i := -1; i <= 1; i++ {
+		dirind := (index + i) % 8
+		if dirind < 0 {
+			dirind = 8 + dirind
+		}
+		dirs.Add(dirscircle[dirind])
+	}
+	return dirs
+}
+
+type pointset map[math.Point]bool
+
+func newPointSet() pointset {
+	return make(map[math.Point]bool)
+}
+
+func newPointSetL(pts []math.Point) pointset {
+	ps := newPointSet()
+	for _, pt := range pts {
+		ps.Add(pt)
+	}
+	return ps
+}
+
+func (ps pointset) Add(pt math.Point) {
+	ps[pt] = true
+}
+
+func (ps pointset) Union(other pointset) pointset {
+	union := newPointSet()
+	for k, v := range ps {
+		union[k] = v
+	}
+	for k, v := range other {
+		union[k] = v
+	}
+	return union
+}
+
+func (ps pointset) Intersect(other pointset) pointset {
+	intersection := newPointSet()
+	for k, v := range ps {
+		if other[k] {
+			intersection[k] = v
+		}
+	}
+	return intersection
 }
--- lib/game/feature_spec.go
+++ lib/game/feature_spec.go
@@ -4,5 +4,5 @@ var (
 	FeatWall       = &Feature{Type: "FeatWall", Solid: true, Opaque: true}
 	FeatFloor      = &Feature{Type: "FeatFloor", Solid: false, Opaque: false}
 	FeatClosedDoor = &Feature{Type: "FeatClosedDoor", Solid: true, Opaque: true}
-	FeatOpenDoor   = &Feature{Type: "FeatOpenDoor", Solid: false, Opaque: true}
+	FeatOpenDoor   = &Feature{Type: "FeatOpenDoor", Solid: false, Opaque: false}
 )
