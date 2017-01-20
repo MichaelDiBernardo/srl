fde1c514d9843f3dadefa2f994dbb34387aac570
Michael Dibernardo <mikedebo@gmail.com>
2017-01-20 11:24:47 +0800

    Testing regen.

--- lib/game/actor_tick.go
+++ lib/game/actor_tick.go
@@ -3,15 +3,12 @@ package game
 import "log"
 
 // We expect a speed 2 actor to fully recover in 100 turns.
-const RegenTurns = 100
+const RegenPeriod = 100
 
 // A thing that keeps track of time passing, and modifies the actor
 // accordingly.
 type Ticker interface {
 	Objgetter
-	// Get ready to tick. This is mostly for the player, as their ticker will
-	// have to work across levels.
-	Init()
 	// The absolute delay that has passed on the level.
 	Tick(delay int)
 }
@@ -28,21 +25,22 @@ func NewActorTicker(obj *Obj) Ticker {
 	return &ActorTicker{Trait: Trait{obj: obj}}
 }
 
-func (t *ActorTicker) Init() {
-	t.last = 0
-}
-
 func (t *ActorTicker) Tick(delay int) {
+	// We've been placed into a new level.
+	if delay < t.last {
+		t.last = 0
+	}
 	diff := delay - t.last
 	t.regen(diff)
 	t.last = delay
 }
 
 func (t *ActorTicker) regen(diff int) {
-	t.regenc += diff
 	sheet := t.obj.Sheet
-	periodt := RegenTurns / sheet.Regen()
-	delayPerHp := periodt * getdelay(2) / sheet.MaxHP()
+	regen := sheet.Regen()
+
+	t.regenc += regen * diff
+	delayPerHp := RegenPeriod * GetDelay(2) / sheet.MaxHP()
 	heal := t.regenc / delayPerHp
 
 	if heal > 0 {
---
+++ lib/game/schedule_test.go
@@ -0,0 +1,64 @@
+package game
+
+import (
+	"testing"
+)
+
+type schedulerTestPair struct {
+	actor string
+	speed int
+}
+
+type schedulerTest struct {
+	actors []schedulerTestPair
+	want   []string
+}
+
+func TestScheduling(t *testing.T) {
+	tests := []schedulerTest{
+		{[]schedulerTestPair{{"A", 1}}, []string{"A", "A", "A", "A", "A"}},
+		{[]schedulerTestPair{{"A", 2}}, []string{"A", "A", "A", "A", "A"}},
+		{[]schedulerTestPair{{"A", 3}}, []string{"A", "A", "A", "A", "A"}},
+		{[]schedulerTestPair{{"A", 4}}, []string{"A", "A", "A", "A", "A"}},
+		{[]schedulerTestPair{{"A", 1}, {"B", 2}}, []string{"B", "A", "B", "A", "B", "B"}},
+		{[]schedulerTestPair{{"A", 1}, {"B", 2}}, []string{"B", "A", "B", "A", "B", "B", "A", "B"}},
+		{[]schedulerTestPair{{"A", 2}, {"B", 4}}, []string{"B", "A", "B", "B", "A", "B", "B", "A", "B", "B", "A"}},
+	}
+
+	g := newTestGame()
+
+	for ti, test := range tests {
+		s := NewScheduler()
+		for _, actorspec := range test.actors {
+			s.Add(lTestSpd(g, actorspec.actor, actorspec.speed))
+		}
+
+		actual := make([]string, 0)
+		for i := 0; i < len(test.want); i++ {
+			actual = append(actual, s.Next().Spec.Name)
+		}
+
+		for si, want := range test.want {
+			if actual[si] != want {
+				t.Errorf("TestScheduler: Test %d -- got %v, want %v", ti, actual, test.want)
+				break
+			}
+		}
+	}
+}
+
+func TestRemoveFromSchedule(t *testing.T) {
+	// TODO
+}
+
+func lTestSpd(g *Game, name string, spd int) *Obj {
+	return g.NewObj(&Spec{
+		Family:  FamActor,
+		Genus:   GenMonster,
+		Species: "TestSpecies",
+		Name:    name,
+		Traits: &Traits{
+			Sheet: NewMonsterSheet(MonsterSheet{speed: spd}),
+		},
+	})
+}
