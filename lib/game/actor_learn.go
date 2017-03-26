ac445fd5bfec3797e59f7784d2fef096b8e953b5
Michael Dibernardo <mikedebo@gmail.com>
2017-03-26 20:28:36 +0545

    Skill learning backend works.

--- lib/game/actor_learn.go
+++ lib/game/actor_learn.go
@@ -1,6 +1,7 @@
 package game
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/MichaelDiBernardo/srl/lib/math"
@@ -18,17 +19,67 @@ type Learner interface {
 	XP() int
 	// How much have they accumulated in total?
 	TotalXP() int
+
+	// Start buying skills. This cannot be called again until CancelLearning or
+	// EndLearning has been called.
+	BeginLearning() (*SkillChange, error)
+	// Buy 1 point of sk. If this costs too much, the existing change will be
+	// returned along with ErrNotEnoughXP.  Otherwise, will return the current
+	// state of the skillchange being requested. This will spend xp and change
+	// skill allocations on the character sheet -- these changes will be
+	// reverted if CancelLearning is called. Will return
+	// ErrNotLearning as err if BeginLearning() was not called before this.
+	LearnSkill(sk SkillName) (*SkillChange, error)
+	// Refund 1 point of sk. This is only allowed if some of sk has already
+	// been bought -- will return the current change and ErrNoPointsLearned if
+	// not. Otherwise, will return the current state of the skillchange being
+	// requested. This will refund xp and change skill allocations on the
+	// character sheet -- these changes will be reverted if CancelLearning is
+	// called. Will return ErrNotLearning as err if BeginLearning() was not
+	// called before this.
+	UnlearnSkill(sk SkillName) (*SkillChange, error)
+	// Cancel the current learning change. This will return the character sheet
+	// back to what it was before the changes were proposed. It is safe to call
+	// BeginLearning again after this has been called.
+	CancelLearning() error
+	// Finalize the learning changes. It is safe to call BeginLearning again
+	// after this has been called.
+	EndLearning() error
 }
 
 // Used by the player to track what they have seen, and how much XP they have.
 type ActorLearner struct {
+	Trait
 	seen   map[Species]int
 	killed map[Species]int
 	xp     int
+	change *SkillChange
 }
 
+// Stores the state of a player's current request to upgrade their skills by
+// spending XP.
+type SkillChange struct {
+	TotalCost int
+	Changes   map[SkillName]SkillChangeItem
+}
+
+type SkillChangeItem struct {
+	Points int
+	Cost   int
+}
+
+var (
+	ErrAlreadyLearning = errors.New("AlreadyLearning")
+	ErrNotLearning     = errors.New("NotLearning")
+	ErrNotEnoughXP     = errors.New("NotEnoughXP")
+	ErrNoPointsLearned = errors.New("NoPointsLearned")
+)
+
+// Implementation of player's learner. Monsters don't need this because they
+// don't gain or spend XP.
 func NewActorLearner(obj *Obj) Learner {
 	return &ActorLearner{
+		Trait:  Trait{obj: obj},
 		seen:   map[Species]int{},
 		killed: map[Species]int{},
 	}
@@ -80,10 +131,102 @@ func (l *ActorLearner) LearnFloor(floor int) {
 	l.xp += floor * 50
 }
 
+func (l *ActorLearner) BeginLearning() (*SkillChange, error) {
+	if l.change != nil {
+		return l.change, ErrAlreadyLearning
+	}
+	l.change = &SkillChange{Changes: map[SkillName]SkillChangeItem{}}
+	return l.change, nil
+}
+
+func (l *ActorLearner) LearnSkill(sk SkillName) (*SkillChange, error) {
+	if l.change == nil {
+		return nil, ErrNotLearning
+	}
+
+	currskill := l.obj.Sheet.UnmodSkill(sk)
+	schange := l.change.Changes[sk]
+
+	cost := skillxp(currskill + 1)
+	newtotal := l.change.TotalCost + cost
+
+	if newtotal > l.XP() {
+		return l.change, ErrNotEnoughXP
+	}
+
+	schange.Points++
+	schange.Cost += cost
+	l.change.Changes[sk] = schange
+	l.change.TotalCost = newtotal
+
+	// Actually modify the player so that these changes will be fully reflected
+	// on the character sheet. If they decide to cancel, we'll revert all
+	// changes.
+	l.xp -= cost
+	l.obj.Sheet.SetSkill(sk, currskill+1)
+
+	return l.change, nil
+}
+
+func (l *ActorLearner) UnlearnSkill(sk SkillName) (*SkillChange, error) {
+	if l.change == nil {
+		return nil, ErrNotLearning
+	}
+
+	schange := l.change.Changes[sk]
+
+	if schange.Points == 0 {
+		return l.change, ErrNoPointsLearned
+	}
+
+	// The current value of the skill will actually have been changed by
+	// LearnSkill(), so we just need to check the unmodded skill.
+	currskill := l.obj.Sheet.UnmodSkill(sk)
+	gain := skillxp(currskill)
+
+	schange.Points -= 1
+	schange.Cost -= gain
+	l.change.Changes[sk] = schange
+	l.change.TotalCost -= gain
+
+	// Actually modify the player so that these changes will be fully reflected
+	// on the character sheet. If they decide to cancel, we'll revert all
+	// changes.
+	l.xp += gain
+	l.obj.Sheet.SetSkill(sk, currskill-1)
+
+	return l.change, nil
+}
+
+func (l *ActorLearner) CancelLearning() error {
+	if l.change == nil {
+		return ErrNotLearning
+	}
+
+	for sk, change := range l.change.Changes {
+		curr := l.obj.Sheet.UnmodSkill(sk)
+		l.obj.Sheet.SetSkill(sk, curr-change.Points)
+	}
+	l.xp += l.change.TotalCost
+
+	l.change = nil
+	return nil
+}
+
+func (l *ActorLearner) EndLearning() error {
+	if l.change == nil {
+		return ErrNotLearning
+	}
+	l.change = nil
+	return nil
+}
+
+// How much XP should you get for seeing or killing mon for the nth time?
 func monxp(mon *Obj, n int) int {
 	return xpdecay(xpfor(mon), n)
 }
 
+// How much xp should you get for seeing item for the nth time?
 func itemxp(item *Obj, n int) int {
 	if n > 0 {
 		return 0
@@ -91,11 +234,18 @@ func itemxp(item *Obj, n int) int {
 	return xpfor(item)
 }
 
+// Raw xp calculation based on native depth.
 func xpfor(obj *Obj) int {
 	depth := obj.Spec.Gen.First()
 	return depth * 10
 }
 
+// Decay XP based on number of times seen / killed.
 func xpdecay(xp, n int) int {
 	return math.Max(1, xp/(n+1))
+}
+
+// How much should the nth point of a skill cost to learn?
+func skillxp(n int) int {
+	return 100 * n
 }
--- lib/game/actor_learn_test.go
+++ lib/game/actor_learn_test.go
@@ -111,3 +111,208 @@ func TestKillXPDecaysForMonsters(t *testing.T) {
 		t.Errorf(`Learner.XP() was %d, want 52`, xp)
 	}
 }
+
+func TestBeginLearningCalledTwiceError(t *testing.T) {
+	g := newTestGame()
+
+	_, err := g.Player.Learner.BeginLearning()
+
+	if err != nil {
+		t.Errorf(`First call to BeginLearning() returned error %v; want nil`, err)
+	}
+
+	_, err = g.Player.Learner.BeginLearning()
+
+	if err != ErrAlreadyLearning {
+		t.Errorf(`Second call to BeginLearning() returned %v; want %v`, err, ErrAlreadyLearning)
+	}
+}
+
+func TestCancelLearningBeforeBeginError(t *testing.T) {
+	g := newTestGame()
+
+	err := g.Player.Learner.CancelLearning()
+
+	if err != ErrNotLearning {
+		t.Errorf(`CancelLearning() returned error %v; want %v`, err, ErrNotLearning)
+	}
+}
+
+func TestEndLearningBeforeBeginError(t *testing.T) {
+	g := newTestGame()
+
+	err := g.Player.Learner.EndLearning()
+
+	if err != ErrNotLearning {
+		t.Errorf(`EndLearning() returned error %v; want %v`, err, ErrNotLearning)
+	}
+}
+
+func TestLearningScenario(t *testing.T) {
+	g := newTestGame()
+	l, s := g.Player.Learner, g.Player.Sheet
+
+	// Set up some base skill investments and mods so that we can test that the
+	// cost only relies on the unmodded skill amount.
+	s.SetSkill(Melee, 1)
+	s.SetSkill(Evasion, 2)
+	s.SetSkill(Shooting, 3)
+	s.ChangeSkillMod(Melee, 1)
+	s.ChangeSkillMod(Evasion, 2)
+	s.ChangeSkillMod(Shooting, 3)
+
+	// Give the player some initial XP to spend.
+	l.(*ActorLearner).xp = 5000
+
+	// Learn some stuff.
+	l.BeginLearning()
+	l.LearnSkill(Melee)            // 200
+	l.LearnSkill(Evasion)          // 300
+	c, _ := l.LearnSkill(Shooting) // 400
+
+	// Player changes
+	if sk, want := s.UnmodSkill(Melee), 2; sk != want {
+		t.Errorf(`s.UnmodSkill(Melee) was %d, want %d`, sk, want)
+	}
+	if sk, want := s.UnmodSkill(Evasion), 3; sk != want {
+		t.Errorf(`s.UnmodSkill(Evasion) was %d, want %d`, sk, want)
+	}
+	if sk, want := s.UnmodSkill(Shooting), 4; sk != want {
+		t.Errorf(`s.UnmodSkill(Shooting) was %d, want %d`, sk, want)
+	}
+	if xp, want := l.XP(), 4100; xp != want {
+		t.Errorf(`l.XP() was %d, want %d`, xp, want)
+	}
+	// Change record upkeep.
+	if cost, want := c.TotalCost, 900; cost != want {
+		t.Errorf(`c.TotalCost was %d, want %d`, cost, want)
+	}
+	if change, want := c.Changes[Melee], (SkillChangeItem{Cost: 200, Points: 1}); change != want {
+		t.Errorf(`c.Changes[Melee] was %+v, want %+v`, change, want)
+	}
+	if change, want := c.Changes[Evasion], (SkillChangeItem{Cost: 300, Points: 1}); change != want {
+		t.Errorf(`c.Changes[Evasion] was %+v, want %+v`, change, want)
+	}
+	if change, want := c.Changes[Shooting], (SkillChangeItem{Cost: 400, Points: 1}); change != want {
+		t.Errorf(`c.Changes[Shooting] was %+v, want %+v`, change, want)
+	}
+
+	c, _ = l.LearnSkill(Melee)
+	if sk, want := s.UnmodSkill(Melee), 3; sk != want {
+		t.Errorf(`s.UnmodSkill(Melee) was %d, want %d`, sk, want)
+	}
+	if xp, want := l.XP(), 3800; xp != want {
+		t.Errorf(`l.XP() was %d, want %d`, xp, want)
+	}
+	if cost, want := c.TotalCost, 1200; cost != want {
+		t.Errorf(`c.TotalCost was %d, want %d`, cost, want)
+	}
+	if change, want := c.Changes[Melee], (SkillChangeItem{Cost: 500, Points: 2}); change != want {
+		t.Errorf(`c.Changes[Melee] was %+v, want %+v`, change, want)
+	}
+
+	l.UnlearnSkill(Melee)
+	c, _ = l.UnlearnSkill(Melee)
+
+	if sk, want := s.UnmodSkill(Melee), 1; sk != want {
+		t.Errorf(`s.UnmodSkill(Melee) was %d, want %d`, sk, want)
+	}
+	if xp, want := l.XP(), 4300; xp != want {
+		t.Errorf(`l.XP() was %d, want %d`, xp, want)
+	}
+	if cost, want := c.TotalCost, 700; cost != want {
+		t.Errorf(`c.TotalCost was %d, want %d`, cost, want)
+	}
+	if change, want := c.Changes[Melee], (SkillChangeItem{}); change != want {
+		t.Errorf(`c.Changes[Melee] was %+v, want %+v`, change, want)
+	}
+
+	l.EndLearning()
+
+	// Check player's final state after learning.
+	if sk, want := s.UnmodSkill(Melee), 1; sk != want {
+		t.Errorf(`s.UnmodSkill(Melee) was %d, want %d`, sk, want)
+	}
+	if sk, want := s.UnmodSkill(Evasion), 3; sk != want {
+		t.Errorf(`s.UnmodSkill(Evasion) was %d, want %d`, sk, want)
+	}
+	if sk, want := s.UnmodSkill(Shooting), 4; sk != want {
+		t.Errorf(`s.UnmodSkill(Shooting) was %d, want %d`, sk, want)
+	}
+	if xp, want := l.XP(), 4300; xp != want {
+		t.Errorf(`l.XP() was %d, want %d`, xp, want)
+	}
+}
+
+func TestCancelLearning(t *testing.T) {
+	g := newTestGame()
+	l, s := g.Player.Learner, g.Player.Sheet
+	l.(*ActorLearner).xp = 5000
+
+	l.BeginLearning()
+	l.LearnSkill(Melee)
+	l.LearnSkill(Melee)
+	l.LearnSkill(Melee)
+
+	l.CancelLearning()
+
+	if sk, want := s.UnmodSkill(Melee), 0; sk != want {
+		t.Errorf(`s.UnmodSkill(Melee) was %d, want %d`, sk, want)
+	}
+	if xp, want := l.XP(), 5000; xp != want {
+		t.Errorf(`l.XP() was %d, want %d`, xp, want)
+	}
+}
+
+func TestCantOverspendXP(t *testing.T) {
+	g := newTestGame()
+	l, s := g.Player.Learner, g.Player.Sheet
+	l.(*ActorLearner).xp = 299
+
+	l.BeginLearning()
+	l.LearnSkill(Melee)
+	c, err := l.LearnSkill(Melee)
+
+	if want := ErrNotEnoughXP; err != want {
+		t.Errorf(`l.LearnSkill() returned err %v, want %v`, err, want)
+	}
+	if sk, want := s.UnmodSkill(Melee), 1; sk != want {
+		t.Errorf(`s.UnmodSkill(Melee) was %d, want %d`, sk, want)
+	}
+	if xp, want := l.XP(), 199; xp != want {
+		t.Errorf(`l.XP() was %d, want %d`, xp, want)
+	}
+	if cost, want := c.TotalCost, 100; cost != want {
+		t.Errorf(`c.TotalCost was %d, want %d`, cost, want)
+	}
+	if change, want := c.Changes[Melee], (SkillChangeItem{Cost: 100, Points: 1}); change != want {
+		t.Errorf(`c.Changes[Melee] was %+v, want %+v`, change, want)
+	}
+}
+
+func TestCantOverRefundXP(t *testing.T) {
+	g := newTestGame()
+	l, s := g.Player.Learner, g.Player.Sheet
+	l.(*ActorLearner).xp = 100
+
+	l.BeginLearning()
+	l.LearnSkill(Melee)
+	l.UnlearnSkill(Melee)
+	c, err := l.UnlearnSkill(Melee)
+
+	if want := ErrNoPointsLearned; err != want {
+		t.Errorf(`l.LearnSkill() returned err %v, want %v`, err, want)
+	}
+	if sk, want := s.UnmodSkill(Melee), 0; sk != want {
+		t.Errorf(`s.UnmodSkill(Melee) was %d, want %d`, sk, want)
+	}
+	if xp, want := l.XP(), 100; xp != want {
+		t.Errorf(`l.XP() was %d, want %d`, xp, want)
+	}
+	if cost, want := c.TotalCost, 0; cost != want {
+		t.Errorf(`c.TotalCost was %d, want %d`, cost, want)
+	}
+	if change, want := c.Changes[Melee], (SkillChangeItem{}); change != want {
+		t.Errorf(`c.Changes[Melee] was %+v, want %+v`, change, want)
+	}
+}
